2a632ec2a7dc6472fc4b36e7f76451d55d4d9ae5
hiyabel belai <hiyabel13@gmail.com>
2023-03-08 12:05:59 -0500

    push

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,6 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [:home]
 
   def home
-  end
-
-  def login
-  end
-
-  def get_started
   end
 end
