0f63404d2119ef8ee6b405597c9039ff8a59f296
michael di fiore <difioremph@gmail.com>
2023-03-09 12:18:32 -0500

    fixed the routs

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,49 +1,15 @@
 class ProjectsController < ApplicationController
-
-  def show
-    @projects = project.find
-
-
 	def new
 		@project = Project.new
 	end
 
-	def create
-		@project = Project.new(project_params)
-		@project.user = current_user
-
-		if @project.save
-      redirect_to project_path(@project)
-    else
-      render :new, status: :unprocessable_entity
-    end
-	end
-
-	def show
-		@project = Project.find(params[:id])
-	end
-
-	private
-
-	def project_params
-		params.require(:project).permit(:name, :description, :industry, :spoken_language_id, :number_of_developers, :user_id)
-	end
-end
-
-
-  def new
-    @project = Project.new
-  end
-
   def show
-    @projects = Project.find(params[:id])
-    @planet.user = current_user
+    @project = Project.find(params[:id])
   end
 
   def create
     @project = Project.new(project_params)
     @project.user = current_user
-
     if @project.save
        redirect_to project_path(@project)
     else
@@ -52,9 +18,7 @@ end
   end
 
   private
-
   def project_params
     params.require(:project).peramit(:name, :description, :industry, :spoken_language_id, :number_of_developers)
-
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,6 @@ Rails.application.routes.draw do
   root to: "pages#home"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :developer, only: [:create, :index, :show, :new]
-  resources :offers, only: [:show, :index, :new]
-  resources :developer, only: [:create, :index, :show, :new]
-  resources :projects, only: [:show]
-  # Defines the root path route ("/")
-  # root "articles#index"
-
-
-  resources :projects, only: [:index, :show, :new, :create] do
-  end
 
   resources :developer, only: [:create, :show, :new]
   resources :offers, only: [:show, :new]
