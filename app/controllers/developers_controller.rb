5d43a273657cb7e5971898ccf782eec8c0525981
paula gendy <paulgendy@gmail.com>
2023-03-09 10:56:12 -0500

    logoimg

--- app/controllers/developers_controller.rb
+++ app/controllers/developers_controller.rb
@@ -1,2 +1,23 @@
 class DevelopersController < ApplicationController
+  def new
+    @developer = DeveloperProfile.new
+  end
+
+  def create
+    @developer = DeveloperProfile.new(developer_params)
+    @developer.user = current_user
+
+    if @developer.save
+      redirect_to developer_path(@developer)
+    else
+      render :new, status: 422
+    end
+  end
+
+  private
+
+  def developer_params
+    params.require(:developer).permit(:school, :degree, :technology, :years_of_experiance)
+  end
+
 end
