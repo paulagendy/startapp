048a28530c552a77a539f6fedb77ee32da08302c
thomas stamour <thomastamour@gmail.com>
2023-03-09 15:05:42 -0500

    Seeds upgrade to have a working data-set

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :developer, only: [:create, :index, :show, :new]
   resources :offers, only: [:show, :index, :new]
   resources :developer, only: [:create, :index, :show, :new]
   resources :projects, only: [:show]
-  # Defines the root path route ("/")
-  # root "articles#index"
-
 
   resources :projects, only: [:index, :show, :new, :create] do
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,46 +1,50 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-User.create(email: 'bob@email.com', password: '123456', role:2,first_name:'Bob', last_name:'Al Koukou' )
-User.create(email: 'anthony@email.com', password: '123456', role:2, first_name:'Anthony', last_name:'Amin' )
-User.create(email: 'Steven@email.com', password: '123456', role:2, first_name:'Steven', last_name:'Andrade' )
-User.create(email: 'samantha@email.com', password: '123456', role:2, first_name:'Samanthat', last_name:'Bruderlein' )
-User.create(email: 'becki@email.com', password: '123456', role:2, first_name:'Becki', last_name:'Belai' )
-User.create(email: 'max@email.com', password: '123456', role:2, first_name:'Max', last_name:'Di Fiore' )
-User.create(email: 'amanda@email.com', password: '123456', role:2, first_name:'Amanda', last_name:'Doulcet' )
-User.create(email: 'matteo@email.com', password: '123456', role:2, first_name:'Matteo', last_name:'Duran' )
-User.create(email: 'jenn@email.com', password: '123456', role:2, first_name:'Jenn', last_name:'Gendy' )
+puts "Cleaning the database"
+
+DeveloperProfile.destroy_all
+ProjectTechnology.destroy_all
+DeveloperProfileSpokenLanguage.destroy_all
+Project.destroy_all
+Technology.destroy_all
+User.destroy_all
+
+bob = User.create!(email: 'bob@email.com', password: '123456', role:2,first_name:'Bob', last_name:'Al Koukou' )
+anthony = User.create!(email: 'anthony@email.com', password: '123456', role:2, first_name:'Anthony', last_name:'Amin' )
+steven = User.create!(email: 'Steven@email.com', password: '123456', role:2, first_name:'Steven', last_name:'Andrade' )
+samantha = User.create!(email: 'samantha@email.com', password: '123456', role:2, first_name:'Samanthat', last_name:'Bruderlein' )
+becki = User.create!(email: 'becki@email.com', password: '123456', role:2, first_name:'Becki', last_name:'Belai' )
+max = User.create!(email: 'max@email.com', password: '123456', role:2, first_name:'Max', last_name:'Di Fiore' )
+amanda = User.create!(email: 'amanda@email.com', password: '123456', role:2, first_name:'Amanda', last_name:'Doulcet' )
+
 # all developers
-User.create(email: 'obonekenobi@email.com', password: '123456', role:1, first_name:'Obone', last_name:'Kenobi' )
-User.create(email: 'markcuban@email.com', password: '123456', role:1, first_name:'Mark', last_name:'Cuban' )
-User.create(email: 'chucknorris@email.com', password: '123456', role:1, first_name:'Chuck', last_name:'Norris' )
+jenn = User.create!(email: 'jenn@email.com', password: '123456', role:2, first_name:'Jenn', last_name:'Gendy' )
+obonekenobi = User.create!(email: 'obonekenobi@email.com', password: '123456', role:1, first_name:'Obone', last_name:'Kenobi' )
+matteo = User.create!(email: 'matteo@email.com', password: '123456', role:2, first_name:'Matteo', last_name:'Duran' )
+markcuban = User.create!(email: 'markcuban@email.com', password: '123456', role:1, first_name:'Mark', last_name:'Cuban' )
+chucknorris = User.create!(email: 'chucknorris@email.com', password: '123456', role:1, first_name:'Chuck', last_name:'Norris' )
 
 # -------------------- Seeds for the projects starts here -------------------- #
-Project.create(name: 'BuzzOn', description: 'A social media platform that connects users with local events and activities based on their interests and location.', industry: 'Social Media');
-Project.create(name: 'GreenWay', description: 'An eco-friendly home cleaning service that uses only sustainable and non-toxic cleaning products.', industry: 'Home Services');
-Project.create(name: 'FoodFuel', description: "A meal delivery service that provides healthy, organic meals tailored to each customer's dietary needs and preferences.", industry: 'Food & Beverage');
-Project.create(name: 'CodeCrush', description: "An online coding bootcamp that teaches beginners how to code and prepares them for a career in tech.", industry: 'E-learning')
-Project.create(name: 'ShopSnap', description: 'A virtual shopping assistant that uses AI to help customers find the products they want and compare prices across multiple retailers.', industry: 'E-commerce')
-Project.create(name: 'FitFlow', description: ' A fitness app that provides personalized workout plans and tracks progress using advanced sensors and machine learning algorithms.', industry: 'Fitness')
-Project.create(name: 'EcoGenius', description: 'A consultancy that helps businesses reduce their environmental impact through sustainability audits and green technology adoption.', industry: 'Consulting')
-Project.create(name: 'SoundScape', description: 'A mobile app that curates personalized playlists for users based on their mood, activity, and music preferences.', industry: 'Music & Entertainment')
+Project.create!(user: bob, name: 'BuzzOn', description: 'A social media platform that connects users with local events and activities based on their interests and location.', industry: 'Social Media');
+Project.create!(user: bob, name: 'GreenWay', description: 'An eco-friendly home cleaning service that uses only sustainable and non-toxic cleaning products.', industry: 'Home Services');
+Project.create!(user: steven, name: 'FoodFuel', description: "A meal delivery service that provides healthy, organic meals tailored to each customer's dietary needs and preferences.", industry: 'Food & Beverage');
+Project.create!(user: steven, name: 'CodeCrush', description: "An online coding bootcamp that teaches beginners how to code and prepares them for a career in tech.", industry: 'E-learning')
+Project.create!(user: samantha, name: 'ShopSnap', description: 'A virtual shopping assistant that uses AI to help customers find the products they want and compare prices across multiple retailers.', industry: 'E-commerce')
+Project.create!(user: samantha, name: 'FitFlow', description: ' A fitness app that provides personalized workout plans and tracks progress using advanced sensors and machine learning algorithms.', industry: 'Fitness')
+Project.create!(user: amanda, name: 'EcoGenius', description: 'A consultancy that helps businesses reduce their environmental impact through sustainability audits and green technology adoption.', industry: 'Consulting')
+Project.create!(user: amanda, name: 'SoundScape', description: 'A mobile app that curates personalized playlists for users based on their mood, activity, and music preferences.', industry: 'Music & Entertainment')
 
 
 # --------------------- Seeds for the developper profile --------------------- #
-DeveloperProfile.create(school: 'University of Toronto', degree: 'Bachelor in Computer Science')
-DeveloperProfile.create(school: 'University of Waterloo', degree: 'Master in Computer Science')
-DeveloperProfile.create(school: 'University of Michigan', degree: 'Certificate in Electrical Engineering')
-DeveloperProfile.create(school: 'University of Texas', degree: 'Master’s in Data Science');
-DeveloperProfile.create(school: 'University of California', degree: 'Master of Science in Artificial Intelligence');
-DeveloperProfile.create(school: 'UC Berkeley', degree: 'Master’s in Cybersecurity');
-DeveloperProfile.create(school: 'Indiana University', degree: 'Master’s Degree in IT Management')
-DeveloperProfile.create(school: 'University of Washington', degree: 'Master’s Degree in Computer Science')
-DeveloperProfile.create(school: 'Syracuse University', degree: 'Master of Science in Information Systems')
-
+DeveloperProfile.create!(user: jenn, school: 'University of Toronto', degree: 'Bachelor in Computer Science')
+DeveloperProfile.create!(user: obonekenobi, school: 'University of Waterloo', degree: 'Master in Computer Science')
+DeveloperProfile.create!(user: matteo, school: 'University of Michigan', degree: 'Certificate in Electrical Engineering')
+DeveloperProfile.create!(user: markcuban, school: 'University of Texas', degree: 'Master’s in Data Science');
+DeveloperProfile.create!(user: chucknorris, school: 'University of California', degree: 'Master of Science in Artificial Intelligence');
+=begin
+DeveloperProfile.create!(school: 'UC Berkeley', degree: 'Master’s in Cybersecurity');
+DeveloperProfile.create!(school: 'Indiana University', degree: 'Master’s Degree in IT Management')
+DeveloperProfile.create!(school: 'University of Washington', degree: 'Master’s Degree in Computer Science')
+DeveloperProfile.create!(school: 'Syracuse University', degree: 'Master of Science in Information Systems')
+=end
 
 techlangs = ["ReactJS", "Qwik", "Astro", "SolidJS", "MongoDB", "Java", "Python", "C++", "C#", "C", "PHP", "Swift", "Kotlin", "Go", "Rust", "Dart", "SQL", "NoSQL", "HTML", "CSS", "JavaScript", "TypeScript", "NodeJS", "Angular", "VueJS", "React Native", "Flutter", "Ionic", "Android", "iOS", "AWS"]
 
