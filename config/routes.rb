96f7da6d613be1e977fb68781250df0ae554ccd1
hiyabel belai <hiyabel13@gmail.com>
2023-03-10 14:04:09 -0500

    Commit temporary changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   resources :developer, only: [:create, :show, :new]
-  resources :offers, only: [:show, :new]
-  resources :projects, only: [:create, :show, :new]
+  resources :offers, only: [:create, :new]
+  resources :projects, only: [:create, :new]
+
 end
