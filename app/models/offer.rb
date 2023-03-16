ead0eae1aa34e628b757e788c2ea5ab0ba6bfead
thomas stamour <thomastamour@gmail.com>
2023-03-16 12:01:05 -0400

    Offer status primary stylings done

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -2,8 +2,8 @@ class Offer < ApplicationRecord
   belongs_to :project
   belongs_to :developer_profile
   enum status: {
-    pending: 0,
-    accepted: 1,
-    rejected: 2,
+    Pending: 0,
+    Accepted: 1,
+    Rejected: 2,
   }
 end
